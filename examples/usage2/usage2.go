6cac80c7955bf3496adfc8d8947a9a975a58cda2
Mohammad Aliyan <md.aliyan@gmail.com>
2018-07-27 14:57:40 +0430

    correct readme mistakes

--- examples/usage2/usage2.go
+++ examples/usage2/usage2.go
@@ -9,15 +9,19 @@ func main() {
 	aString := govert.ToString(3783.2882332, 3) // "3783.288"
 
 	// bool to string
-	aTrueString := govert.ToFloat32(true)  // "1"
-	aFalseString := govert.ToFloat32(false)  // "0"
+	aTrueString := govert.ToString(true)   // "true"
+	aFalseString := govert.ToString(false) // "false"
+
+	// bool to string
+	aTrueInt8 := govert.ToInt8(true)   // 1
+	aFalseInt8 := govert.ToInt8(false) // 0
 
 	// string to float64
-	aFloat32 := govert.ToFloat32("3783.2882332")  // "3783.288"
+	aFloat32 := govert.ToFloat32("3783.2882332") // 3783.2883
 
 	// string to int8
-	anInt8 := govert.ToInt8("56")  // "56"
+	anInt8 := govert.ToInt8("56") // 56
 
-	fmt.Println(aString, aTrueString, aFalseString, aFloat32, anInt8)
-	//3783.288 1 0 3783.2883 56
+	fmt.Println(aString, aTrueString, aFalseString, aTrueInt8, aFalseInt8, aFloat32, anInt8)
+	// 3783.288 true false 1 0 3783.2883 56
 }
